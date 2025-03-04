44ce4606db6570028285d30a6151d01489a8497d
jamesanton1213 <jamesanthonyragcabiscarra@gmail.com>
2025-03-04 22:36:09 +0800

    activity1.js

---
+++ variables.js
@@ -0,0 +1,9 @@
+let name = 'Anthony James';
+let whatDoYouWannaBecomeInYourLife = 'programmer';
+let gender = 'Male';
+let twitterHandle = 'Anthojams';
+
+console.log(name);
+console.log(whatDoYouWannaBecomeInYourLife);
+console.log(gender);
+console.log(twitterHandle);
